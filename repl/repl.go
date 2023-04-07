1fd386bfd398e9a9520a6bb0fd5486ee60df19a3
KishorPokharel <kishorpokharel7@gmail.com>
2023-04-07 06:00:41 +0545

    evaluate in repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/KishorPokharel/monkey/evaluator"
 	"github.com/KishorPokharel/monkey/lexer"
-	"github.com/KishorPokharel/monkey/token"
+	"github.com/KishorPokharel/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +24,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
